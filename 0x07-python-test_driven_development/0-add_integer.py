9ab2cf78d2155037dc1b5c7333172a6821ce3221
Iamouma <stephenouma452@gmail.com>
2024-01-03 01:19:00 -0800

    More files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""this module has the function add_integer"""
+
+
+def add_integer(a, b=98):
+    """
+    Returns the sum of a and b
+
+    >>>add_integer(3, 5)
+    8
+
+    Raises type error if arguments are not int or float type
+    """
+    if type(a) == float:
+        """cast float to integers"""
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    elif type(a) != int:
+        """raise typeerrorwhen number is not a float or integer"""
+        raise TypeError("a must be an integer")
+    elif type(b) != int:
+        raise TypeError("b must be an integer")
+    return a + b
