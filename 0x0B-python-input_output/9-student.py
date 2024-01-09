c93f1f86641d4b7dfe8999d0263c9b099a7e6508
Iamouma <stephenouma452@gmail.com>
2024-01-09 12:58:20 -0800

    A class Student

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines a class Student"""
+
+
+class Student:
+    """This represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initializes a new Student.
+        Args:
+            first_name (str): Is the first name of the Student.
+            last_name (str): Is the last name of the Student.
+            age (int): Is the age of the Student.
+        """
+        self.first_name = first_name
+        self.last_nmae = last_name
+        self.age = age
+
+    def to_json(self):
+        """This gets a dictionary representation of the Student,"""
+        return self.__dict__
