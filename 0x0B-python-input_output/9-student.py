dbce2803c790a24e68eba60c88f971d92da44673
Iamouma <stephenouma452@gmail.com>
2024-01-09 13:02:02 -0800

    A class Student

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -13,7 +13,7 @@ class Student:
             age (int): Is the age of the Student.
         """
         self.first_name = first_name
-        self.last_nmae = last_name
+        self.last_name = last_name
         self.age = age
 
     def to_json(self):
