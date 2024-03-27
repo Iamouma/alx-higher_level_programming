35ae4944bbe391bf73e5fb37d4fc8c1816d18e14
Iamouma <stephenouma452@gmail.com>
2024-03-27 11:53:38 -0700

    A Bash script that takes in a URL, sends a POST request to the passed URL, and displays the body of the response

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to the passed URL, and displays the body of the reponse
-curl -sX "POST" "$1" -d "email=test@gmail.com&subject=I will post the PLD link"
+curl -sX "POST" "$1" -d "email=test@gmail.com&subject=I will always be here for PLD"
