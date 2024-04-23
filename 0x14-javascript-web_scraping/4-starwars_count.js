e38c86bfd2905378c19057150002930a2c0c42fb
Iamouma <stephenouma452@gmail.com>
2024-04-23 02:56:03 -0700

    A script that gets the contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = procees.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body).results;
+
+    const moviesWithWedge = results.reduce((count, movie) => {
+      return movie.characters.find((characters) => character.endsWith('/18/'))
+        ? count + 1
+        : count;
+    }, 0);
+    console.log(moviesWithWegde);
+  }
+});
